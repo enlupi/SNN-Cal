ca494393ac10f7fcd34732375819062883e2b13a
Cloud User <centos@snn-hgcal.novalocal>
2025-01-03 18:13:53 +0100

    ADD multithread

--- GenerateDataset/genPhotons.cpp
+++ GenerateDataset/genPhotons.cpp
@@ -6,12 +6,17 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cstdio>
+#include <thread>
 #include "TMath.h"
 #include "TFile.h"
 #include "TTree.h"
 #include "TVector3.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
 
 using namespace std;
+
 
 enum Particle {
   unclassified = 0,
@@ -21,7 +26,27 @@ enum Particle {
   other = 4
 };
 
-// set useful constant values
+
+vector<float> read_matrices(string filename);
+int total_reflections(int n);
+void processFile(string file_name, string tree_name, string output_file_path,
+                 vector<float>& emission_matrix, int max_N,
+                 int verbose=0, bool primary_only=true, int max_events=1000);
+void processEntries(TTree* tree, int start_evt, int end_evt, int total_points,
+                    vector<float>& emission_matrix, string output_file_name, bool primary_only);
+void saveEvent(string filename, vector<vector<vector<int>>>& photon_matrix,
+               vector<double>& Etot, vector<TVector3>& Ecentroid, vector<TVector3>& sigmaE,
+               vector<int>& Nint, vector<Particle>& p,
+               int primary_peak_cub, bool primary_only);
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                           CONSTANTS                                       //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
 int nCubletsX = 10, nCubletsY = 10, nCubletsZ = 10;
 int nCellsXY = 10;
 int nCellsZ  = 10;
@@ -37,17 +62,12 @@ double dt = 0.2; // ns
 int timesteps = max_t/dt;
 
 
-// input tree variables
-int i_evt;
-int n_int;
-vector<int>*    pdg;
-vector<double>* edep;
-vector<double>* deltae;
-vector<double>* glob_t;
-vector<int>*    cublet_idx;
-vector<int>*    cell_idx;
 
-
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                  FUNCTIONS TO READ EMISSION MATRIX                        //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
 
 vector<float> read_matrices(string filename){
     // Step 1: Read the shape from the text file
@@ -108,34 +128,110 @@ int total_reflections(int n){
 }
 
 
-void genPhotonTree(string filename, string treename, string outputFilePath,
-                   vector<float>& emission_matrix, int max_N,
-                   int verbose=0, bool primary_only=true, int max_event=1000) {
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                FUNCTIONS TO PROCESS FILES IN PARALLEL                     //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+void processFile(string file_name, string tree_name, string output_file_path,
+                 vector<float>& emission_matrix, int max_N,
+                 int verbose, bool primary_only, int max_events) {
 
   auto start_time = std::chrono::high_resolution_clock::now();
 
-  int total_points = total_reflections(max_N);
-  vector<int> shape{nCellsXY, nCellsXY, nCellsZ, total_reflections(5), nCellsXY, nCellsZ, 2};
-  int dims = shape.size();
-
-  size_t name_start = filename.find_last_of('/');
-  size_t name_end   = filename.find_last_of('.');
-  string name = filename.substr(name_start+1, name_end-name_start-1);
+  size_t name_start = file_name.find_last_of('/');
+  size_t name_end   = file_name.find_last_of('.');
+  string name = file_name.substr(name_start+1, name_end-name_start-1);
+  string output_file_name = output_file_path + name;
 
   // get simulation results tree from file
-  TFile* file = TFile::Open(filename.c_str()); 
-  TTree* tree = (TTree*) file->Get(treename.c_str())->Clone(); 
+  TFile* file = TFile::Open(file_name.c_str()); 
+  TTree* tree = (TTree*) file->Get(tree_name.c_str())->Clone(); 
 
-  tree->SetBranchAddress("Tevent_id",       &i_evt);
-  tree->SetBranchAddress("Tinteractions_in_event", &n_int);
-  tree->SetBranchAddress("Tpdg",            &pdg);
-  tree->SetBranchAddress("Tedep",           &edep);
-  tree->SetBranchAddress("Tdeltae",         &deltae);
-  tree->SetBranchAddress("Tglob_t",         &glob_t);
-  tree->SetBranchAddress("Tcublet_idx",     &cublet_idx);
-  tree->SetBranchAddress("Tcell_idx",       &cell_idx);
+  int total_points = total_reflections(max_N);
 
+  // Handle threads
+  std::function<void(TTree*, int, int, int, std::vector<float>&, std::string, bool)> func =
+        processEntries;
+  int n_threads = std::thread::hardware_concurrency();
+  int entries_per_thread = max_events / n_threads;
+  vector<thread> threads;
+
+  for (int t = 0; t < n_threads; ++t) {
+    int start = t*entries_per_thread;
+    int end = (t == n_threads - 1) ? max_events : start + entries_per_thread;
+
+    //threads.emplace_back(func, tree, start, end, total_points,
+    //                     emission_matrix, output_file_name, primary_only);
+    threads.emplace_back( [&]() {
+      processEntries(tree, start, end, total_points, emission_matrix, output_file_name, primary_only);
+    });
+  }
+
+  // Join threads
+  for (auto& t : threads) {
+    t.join();
+  }
+
+  // merge output files if only primary cublet is saved
+  if(primary_only){
+
+    // create output file
+    ofstream outfile(output_file_name + ".dat", std::ios::binary);
+    if (!outfile) {
+      cerr << "Error opening file for writing!" << endl;
+      return;
+    }
+
+    // append results of each event to final result file
+    for(int i = 0; i < max_events; i++) {
+      string temp_file_name = output_file_name + "_" + to_string(i) + ".dat";
+      ifstream tempfile(temp_file_name, std::ios::binary);
+      if(!tempfile) {
+        cerr << "Error: Could not open temporary input file: " << tempfile << endl;
+        continue;  // Skip this file if we can't open it
+      }
+      outfile << tempfile.rdbuf();  // Append the contents of the temporary file
+      tempfile.close();
+      remove(temp_file_name.c_str());  // Delete the temporary file
+    }
+
+    outfile.close();
+  }
+
+  file->Close();
+
+  // Stop measuring time
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+  if(verbose) {
+    cout << "Finished processing file. Time taken: " << duration.count() << " secs. " << std::endl;
+  }
   
+  return;
+}
+
+
+
+void processEntries(TTree* tree, int start_evt, int end_evt, int total_points,
+                    vector<float>& emission_matrix, string output_file_name, bool primary_only) {
+
+  vector<int> shape{nCellsXY, nCellsXY, nCellsZ, total_reflections(5), nCellsXY, nCellsZ, 2};
+  int dims = shape.size();
+  
+  // Create a TTreeReader for this thread
+  TTreeReader reader(tree);
+  TTreeReaderValue<int> n_int(reader, "Tinteractions_in_event");
+  TTreeReaderValue<vector<int>> pdg(reader, "Tpdg");
+  TTreeReaderValue<vector<double>> edep(reader, "Tedep");
+  TTreeReaderValue<vector<double>> deltae(reader, "Tdeltae");
+  TTreeReaderValue<vector<double>> glob_t(reader, "Tglob_t");
+  TTreeReaderValue<vector<int>> cublet_idx(reader, "Tcublet_idx");
+  TTreeReaderValue<vector<int>> cell_idx(reader, "Tcell_idx");
+
+  // create vectors to store info per event
   vector<vector<vector<int>>> photon_matrix(nCublets,  vector<vector<int>>(
                                             timesteps, vector<int>(
                                             n_sensors, 0)));
@@ -148,27 +244,9 @@ void genPhotonTree(string filename, string treename, string outputFilePath,
   vector<int> pdg_max(nCublets, 0);                      // pdg encoding of primary particle...
                                                          // per cublet 
 
-  ofstream outfile;
-  if(primary_only){
-    outfile.open(outputFilePath + name + ".dat", std::ios::binary);
-    if (!outfile) {
-      cerr << "Error opening file for writing!" << endl;
-      return;
-    }
-  }
-
-  // loop over events
-  int n_evts = (int) tree->GetEntries(); // should be 1000
-  if(max_event < n_evts){
-    n_evts = max_event;
-  }
-  for (int i = 0; i < n_evts; i++) {
-
-    if(verbose) {
-      cout << "\tProcessing event " << i << "..." << endl;
-    }
-
-    tree->GetEntry(i);
+  // loop over events in batch
+  for(int i = start_evt; i < end_evt; i++){
+    reader.SetEntry(i);
 
     // reset values
     for(auto& matrix: photon_matrix) {
@@ -189,7 +267,7 @@ void genPhotonTree(string filename, string treename, string outputFilePath,
     int primary_peak_cub = -1;
     
     // loop over interaction per event
-    for (int j = 0; j < n_int; j++) {
+    for (int j = 0; j < *n_int; j++) {
 
       // check if energy has been deposited before max time
       double E  = (*edep)[j];
@@ -208,9 +286,6 @@ void genPhotonTree(string filename, string treename, string outputFilePath,
         // check if vertex is primary
         if(dE < deltaE_vtx_thr && dE < dE_primary) { // less than, as they are negative
           primary_peak_cub = cub_i;
-          if(verbose > 1){
-            cout << "\tPrimary vtx - pdg: " << (*pdg)[j]  << " - E: " << E << " - dE: " << dE << ";";
-          }
         }
 
         // check maximum energy difference inside cublet
@@ -292,7 +367,7 @@ void genPhotonTree(string filename, string treename, string outputFilePath,
     }
 
     // compute energy dispersions
-    for (int j = 0; j < n_int; j++) {
+    for (int j = 0; j < *n_int; j++) {
       int cub_i = (*cublet_idx)[j];
 
       // check if energy has been released in the cublet, otherwise skip
@@ -311,168 +386,97 @@ void genPhotonTree(string filename, string treename, string outputFilePath,
     }
 
     // save photon counts to file
-    if(!primary_only){
-      outfile.open(outputFilePath + name + "_" + to_string(i) + ".dat", std::ios::binary);
-      if (!outfile) {
-        cerr << "Error opening file for writing during event " << i << "!" << endl;
-        return;
-      }
-    }
-
-    // Write data to file
-    for(int i_cub = 0; i_cub < nCublets; i_cub++) {
-
-      // check if energy has been released in the cublet, otherwise skip
-      if((primary_only && (i_cub != primary_peak_cub)) || !(Etot[i_cub] > 0)) continue;
-
-      // photon_matrix data
-      for(int i_t = 0; i_t < timesteps; i_t++){
-        for(int i_s = 0; i_s < n_sensors; i_s++){
-          int entry = photon_matrix[i_cub][i_t][i_s];
-          if(entry != 0){
-            outfile.write(reinterpret_cast<char*>(&i_t), sizeof(i_t));
-            outfile.write(reinterpret_cast<char*>(&i_s), sizeof(i_s));
-            outfile.write(reinterpret_cast<char*>(&entry), sizeof(entry));
-          }
-        }
-      }
-
-      // print stop bits
-      int stop = 2147483647;
-      outfile.write(reinterpret_cast<char*>(&stop), sizeof(stop));
-
-      // cublet index
-      outfile.write(reinterpret_cast<char*>(&i_cub), sizeof(i_cub));
-
-      // total energy
-      outfile.write(reinterpret_cast<char*>(&Etot[i_cub]), sizeof(Etot[i_cub]));
-      
-      // energy centroid
-      double x = Ecentroid[i_cub].X();
-      double y = Ecentroid[i_cub].Y();
-      double z = Ecentroid[i_cub].Z();
-      outfile.write(reinterpret_cast<char*>(&x), sizeof(x));
-      outfile.write(reinterpret_cast<char*>(&y), sizeof(y));
-      outfile.write(reinterpret_cast<char*>(&z), sizeof(z));
-
-      // energy dispersion
-      double sX = sigmaE[i_cub].X()/Etot[i_cub];
-      double sY = sigmaE[i_cub].Y()/Etot[i_cub];
-      double sZ = sigmaE[i_cub].Z()/Etot[i_cub];
-      outfile.write(reinterpret_cast<char*>(&sX), sizeof(sX));
-      outfile.write(reinterpret_cast<char*>(&sY), sizeof(sY));
-      outfile.write(reinterpret_cast<char*>(&sZ), sizeof(sZ));
-
-      // number of interactions
-      outfile.write(reinterpret_cast<char*>(&Nint[i_cub]), sizeof(Nint[i_cub]));
-
-      // particle info
-      outfile.write(reinterpret_cast<char*>(&p[i_cub]), sizeof(p[i_cub]));
-
-      // primary vertex
-      if(!primary_only){
-        int is_primary = (i_cub == primary_peak_cub);
-        outfile.write(reinterpret_cast<char*>(&is_primary), sizeof(is_primary));
-      }
-
-      if(verbose > 1){
-        cout << endl;
-        cout << "\ti_cub: " << i_cub << " - Etot: " << Etot[i_cub] << " - dEmax: " << dEmax[i_cub] 
-             << " - pdg_max: " << pdg_max[i_cub] << " - classification: " << p[i_cub] << endl;
-      }
-    }
-
-    // Close the file
-    if(!primary_only){
-      outfile.close();
-    }
+    saveEvent(output_file_name + "_" + to_string(i) + ".dat",
+              photon_matrix, Etot, Ecentroid, sigmaE, Nint, p,
+              primary_peak_cub, primary_only);
   }
 
-  // Close the file
-  if(primary_only){
-    outfile.close();
-  }
-
-  file->Close();
-
-  // Stop measuring time
-  auto end_time = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
-  if(verbose) {
-    cout << "Finished processing file. Time taken: " << duration.count() << " secs. " << std::endl;
-  }
-  
   return;
 }
 
 
 
+void saveEvent(string filename, vector<vector<vector<int>>>& photon_matrix,
+               vector<double>& Etot, vector<TVector3>& Ecentroid, vector<TVector3>& sigmaE,
+               vector<int>& Nint, vector<Particle>& p,
+               int primary_peak_cub, bool primary_only) {
 
-
-double onAxis_SolidAngle(double a, double b, double d) {
-  double alpha = a/(2*d);
-  double beta  = b/(2*d);
-  return 4*TMath::ASin(alpha*beta/TMath::Sqrt((1+alpha*alpha)*(1+beta*beta))); 
-}
-
-double offAxis_SolidAngle(double A, double B, double a, double b, double d) {
-  double sign_A = 1;
-  if(A < 0) {
-    sign_A = -1;
-    A *= -1;
+  // create output file
+  ofstream outfile(filename, std::ios::binary);
+  if (!outfile) {
+    cerr << "Error opening file " << filename << " for writing results!" << endl;
+    return;
   }
-  double sign_B = 1;
-  if(B < 0) {
-    sign_B = -1;
-    B *= -1;
-  }
-  double omega1 = onAxis_SolidAngle(2*(a+sign_A*A), 2*(b+sign_B*B), d);
-  double omega2 = onAxis_SolidAngle(2*A,            2*(b+sign_B*B), d);
-  double omega3 = onAxis_SolidAngle(2*(a+sign_A*A), 2*B,            d);
-  double omega4 = onAxis_SolidAngle(2*A,            2*B,            d);
-
-  double omega = (omega1 - sign_A*omega2 - sign_B*omega3 + sign_A*sign_B*omega4)/4;
-
-  return omega;
-}
-
-// assumes sensors on upper xz plane
-vector<vector<vector<vector<double>>>> create_matrices(double cellSizeX, double cellSizeY, double cellSizeZ,
-                                                       int nCellsX, int nCellsY, int nCellsZ) {
-
-  // light speed
-  double n = 2.2; // PWO refractive index
-  double c0 = 299.792458/n; // mm/ns
-  double c = c0/n;
-
-  vector<vector<vector<double>>> angle_matrix(nCellsX, vector<vector<double>>(nCellsY, vector<double>(nCellsZ, 0)));
-  vector<vector<vector<double>>>  time_matrix(nCellsX, vector<vector<double>>(nCellsY, vector<double>(nCellsZ, 0)));
   
-  // loop over y
-  for(int j = 0; j < nCellsY; j++) {
-    TVector3 start_point(0, j*cellSizeY, 0);
+  // Write data to file
+  for(int i_cub = 0; i_cub < nCublets; i_cub++) {
 
-    // loop over z
-  	for(int k = 0; k < nCellsZ; k++) {
-      // loop over x 
-      for(int i = 0; i < nCellsX; i++) {
-        TVector3 end_point(i*cellSizeX, (nCellsY-0.5)*cellSizeY, k*cellSizeZ);
+    // check if energy has been released in the cublet, otherwise skip
+    if((primary_only && (i_cub != primary_peak_cub)) || !(Etot[i_cub] > 0)) continue;
 
-        double d = end_point.Y() - start_point.Y();
-        double A = end_point.X() - cellSizeX/2;
-        double B = end_point.Z() - cellSizeZ/2;
-        angle_matrix[i][j][k] = offAxis_SolidAngle(A, B, cellSizeX, cellSizeZ, d);
-        time_matrix[i][j][k] = (end_point - start_point).Mag()/c;
+    // photon_matrix data
+    for(int i_t = 0; i_t < timesteps; i_t++){
+      for(int i_s = 0; i_s < n_sensors; i_s++){
+        int entry = photon_matrix[i_cub][i_t][i_s];
+        if(entry != 0){
+          outfile.write(reinterpret_cast<char*>(&i_t), sizeof(i_t));
+          outfile.write(reinterpret_cast<char*>(&i_s), sizeof(i_s));
+          outfile.write(reinterpret_cast<char*>(&entry), sizeof(entry));
+        }
       }
-    }      
-  } 
-  vector<vector<vector<vector<double>>>> result;
-  result.push_back(angle_matrix);
-  result.push_back(time_matrix);
+    }
 
-  return result;
+    // print stop bits
+    int stop = 2147483647;
+    outfile.write(reinterpret_cast<char*>(&stop), sizeof(stop));
+
+    // cublet index
+    outfile.write(reinterpret_cast<char*>(&i_cub), sizeof(i_cub));
+
+    // total energy
+    outfile.write(reinterpret_cast<char*>(&Etot[i_cub]), sizeof(Etot[i_cub]));
+      
+    // energy centroid
+    double x = Ecentroid[i_cub].X();
+    double y = Ecentroid[i_cub].Y();
+    double z = Ecentroid[i_cub].Z();
+    outfile.write(reinterpret_cast<char*>(&x), sizeof(x));
+    outfile.write(reinterpret_cast<char*>(&y), sizeof(y));
+    outfile.write(reinterpret_cast<char*>(&z), sizeof(z));
+
+    // energy dispersion
+    double sX = sigmaE[i_cub].X()/Etot[i_cub];
+    double sY = sigmaE[i_cub].Y()/Etot[i_cub];
+    double sZ = sigmaE[i_cub].Z()/Etot[i_cub];
+    outfile.write(reinterpret_cast<char*>(&sX), sizeof(sX));
+    outfile.write(reinterpret_cast<char*>(&sY), sizeof(sY));
+    outfile.write(reinterpret_cast<char*>(&sZ), sizeof(sZ));
+
+    // number of interactions
+    outfile.write(reinterpret_cast<char*>(&Nint[i_cub]), sizeof(Nint[i_cub]));
+
+    // particle info
+    outfile.write(reinterpret_cast<char*>(&p[i_cub]), sizeof(p[i_cub]));
+
+    // primary vertex
+    if(!primary_only){
+      int is_primary = (i_cub == primary_peak_cub);
+      outfile.write(reinterpret_cast<char*>(&is_primary), sizeof(is_primary));
+    }
+  }
+
+  outfile.close();
+
+  return;
 }
 
+
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                                MAIN                                       //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char* argv[]) {
 
@@ -558,7 +562,7 @@ int main(int argc, char* argv[]) {
        << "Analyzing file " << fileName << ":" << endl;
 
 
-  genPhotonTree(fileName, "outputTree", outputFilePath, emission_matrix,
+  processFile(fileName, "outputTree", outputFilePath, emission_matrix,
                 reflections, verbose, primary_only, max_event);
 
 	cout << "File processing completed." << endl;
